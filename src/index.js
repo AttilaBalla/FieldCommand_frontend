81236a19f6f7b3f67bf4c49b36e725eaeceed4c1
AttilaBalla <xattus@gmail.com>
2018-05-17 14:26:28 +0200

    login page added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Main } from './js/App';
+import { Login } from './js/Login'
 import './css/main.css';
 import registerServiceWorker from './js/registerServiceWorker';
 
 render(
-
     <Router>
-        <Main/>
+        <Switch>
+            <Route exact path='/login' component={Login}/>
+            <Route path='/' component={Main}/>
+        </Switch>
     </Router>,
     document.getElementById('root'),
     registerServiceWorker()
