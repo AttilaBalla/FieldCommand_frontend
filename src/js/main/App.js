a96bd84d4971b457373a7e48dd6624dffbf69de4
AttilaBalla <xattus@gmail.com>
2018-06-08 07:40:20 +0200

    route for activate component added

--- src/js/main/App.js
+++ src/js/main/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {Login} from "../util/Login";
 import {Main} from "../core/Cores.js"
 import {Provider} from "../util/UserProvider";
+import {Activate} from "../util/Activate";
 
 export class App extends React.Component {
     render() {
@@ -10,6 +11,7 @@ export class App extends React.Component {
             <Provider>
                 <Router>
                     <Switch>
+                        <Route path='/activate/:id' component={Activate}/>
                         <Route exact path='/login' component={Login}/>
                         <Route path='/' component={Main}/>
                     </Switch>
