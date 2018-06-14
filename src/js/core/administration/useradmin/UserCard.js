1c1c98e99719c705be55e6b67744734c2d77bd95
AttilaBalla <xattus@gmail.com>
2018-06-14 14:38:28 +0200

    Useredit validation frontend added

--- src/js/core/administration/useradmin/UserCard.js
+++ src/js/core/administration/useradmin/UserCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import {UserRoleElement, userRoles} from "./UserRoleElement"
 import {updateUser} from "../../../util/APIUtils";
 import {alertTypes} from "../../../util/Alert";
+import {UserContext} from "../../../util/UserProvider";
+import {UserCardForm} from "./UserCardForm";
 
 export class UserCard extends React.Component {
 
@@ -13,7 +15,8 @@ export class UserCard extends React.Component {
             id: props.id,
             username: props.name,
             email: props.email,
-            role: props.role
+            role: props.role,
+            rolePower: props.rolePower,
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,6 +28,10 @@ export class UserCard extends React.Component {
         let nameColor = "text-dark";
 
         switch(role) {
+
+            case "ROLE_OWNER":
+                nameColor = "text-owner";
+                break;
 
             case "ROLE_ADMIN":
                 nameColor = "text-danger";
@@ -47,11 +54,9 @@ export class UserCard extends React.Component {
                 break;
             default:
                 break;
-
         }
 
         return nameColor;
-
     }
 
     handleChange(event) {
@@ -69,80 +74,67 @@ export class UserCard extends React.Component {
                 });
 
             }).catch(error => {
-            if (error.status === 401) {
-
-                this.props.sendAlert({
-                    alertType: alertTypes.ERROR,
-                    message: "You do not have permission to modify that user!"
-                });
-            } else {
-
                 this.props.sendAlert({
                     alertType: alertTypes.ERROR,
                     message: error.information
                 });
-            }
-        })
-
+            })
     }
 
     render() {
 
-        let roleElements = userRoles.map((role, key) => {
-            return (
-                <UserRoleElement
-                    key = {key}
-                    role = {role.roleName}
-                    checked = {(this.state.role === role.roleName)}
-                    badgeColor = {role.badgeColor}
-                    displayName = {role.displayName}
-                    handleChange = {this.handleChange}
-                />
-            )
-        });
+        let rolePanel = null;
+        if (this.props.role === "ROLE_OWNER") {
+            rolePanel = <p className="text-secondary">
+                            <i className="fa fa-ban mr-2" aria-hidden="true"></i>
+                            cannot be changed for this user!
+                        </p>
+        } else {
+            rolePanel = userRoles.map((role, key) => {
+                return (
+                    <UserRoleElement
+                        key={key}
+                        role={role.roleName}
+                        checked={(this.state.role === role.roleName)}
+                        badgeColor={role.badgeColor}
+                        displayName={role.displayName}
+                        handleChange={this.handleChange}
+                    />
+                )
+            });
+        }
 
         return (
-            <div className="card">
-                <div className="card-header collapsed" data-toggle="collapse" data-target={"#collapse" + this.state.index}>
-                    <h6 className={"mb-0 " + this.setNameColor(this.state.role)} >{this.state.username}</h6>
-                </div>
-                <div className="collapse" id={"collapse" + this.state.index} data-parent="#accordion">
-                    <div className="card-body">
-                        <form className="user_edit" onSubmit={this.handleSubmit}>
-                            <div className="float-left user_details">
-                                <label htmlFor="username">Username</label>
-                                <input type="text"
-                                       name="username"
-                                       className="form-control user_username"
-                                       onChange={this.handleChange}
-                                       defaultValue={this.state.username}/>
-                                <label htmlFor="email" className="mt-3">E-mail address</label>
-                                <input type="text"
-                                       name="email"
-                                       className="form-control user_email"
-                                       onChange={this.handleChange}
-                                       defaultValue={this.state.email}/>
+            <UserContext.Consumer>
+                {value => {
+                    const {user} = value;
+                    let editable = true;
+                    if(user.id !== parseInt(this.state.id, 10)) {
+                        if(user.rolePower <= this.state.rolePower) {
+                            editable = false;
+                        }
+                    }
+
+                    return(
+                        <div className="card">
+                            <div className="card-header collapsed" data-toggle="collapse" data-target={"#collapse" + this.state.index}>
+                                <h6 className={"mb-0 " + this.setNameColor(this.state.role)} >{this.state.username}</h6>
                             </div>
-                            <div className="float-right mt-2 user_roles">
-                                <h6>Roles</h6>
-                                {roleElements}
+                            <div className="collapse" id={"collapse" + this.state.index} data-parent="#accordion">
+                                {(editable)
+                                    ? <UserCardForm
+                                        submit={this.handleSubmit}
+                                        change={this.handleChange}
+                                        username={this.state.username}
+                                        email={this.state.email}
+                                        rolepanel={rolePanel}
+                                    />
+                                    : null}
                             </div>
-                            <div className="float-right mt-2 user_versions">
-                                <h6> Versions </h6>
-                                <input type="checkbox" name="versions" value="Official"/>
-                                <span className="badge badge_version ml-2">Official</span>
-                                <input type="checkbox" name="versions" value="BP 1.5"/>
-                                <span className="badge badge_version ml-2">BP 1.5</span>
-                            </div>
-                            <div className="float-right w-100">
-                                <button className="btn btn-warning user_update_button float-right mb-3" type="submit">
-                                    Save changes
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                        </div>
+                    )
+                }}
+            </UserContext.Consumer>
         )
     }
 }
