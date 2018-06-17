88203c1595817069bdcbc17aa8b71639d3244529
AttilaBalla <xattus@gmail.com>
2018-06-17 18:10:06 +0200

    News element added + refactor

--- src/js/core/administration/SidebarItem.js
+++ src/js/core/administration/SidebarItem.js
@@ -1,77 +1,48 @@
 import React from 'react';
 
-export const sidebarTypes = {
-    TITLE: {
-        classElement: "centered font-weight-bold sidebar_title",
-        text: "Administration"
-    },
-    GENERAL: {
+
+export const sidebarTypes = [
+    {
         classElement: "nav-item sidebar_button",
         icon: "fa fa-cogs",
         text: "General"
     },
-    GAMEREPORTS: {
+    {
+        classElement: "nav-item sidebar_button",
+        icon: "fa fa-file-text-o",
+        text: "Newsfeed"
+    },
+    {
         classElement: "nav-item sidebar_button",
         icon: "fa fa-fire-extinguisher",
         text: "Game Reports"
     },
-    RELEASES: {
+    {
         classElement: "nav-item sidebar_button",
         icon: "fa fa-upload",
         text: "Releases"
     },
-    USERS: {
+    {
         classElement: "nav-item sidebar_button",
         icon: "fa fa-user",
         text: "Users"
     }
-};
+];
 
 export class SidebarItem extends React.Component{
 
     constructor(props) {
         super(props);
 
-        this.classElement = "";
-        this.icon = "";
-        this.text = "";
+        this.classElement = props.classElement;
+        this.icon = props.icon;
+        this.text = props.text;
 
         this.handleClick = this.handleClick.bind(this);
     }
 
-    componentWillMount() {
-        switch (this.props.itemType) {
-            case sidebarTypes.TITLE:
-                this.classElement = sidebarTypes.TITLE.classElement;
-                this.text = sidebarTypes.TITLE.text;
-                break;
-            case sidebarTypes.GENERAL:
-                this.classElement = sidebarTypes.GENERAL.classElement;
-                this.icon = sidebarTypes.GENERAL.icon;
-                this.text = sidebarTypes.GENERAL.text;
-                break;
-            case sidebarTypes.GAMEREPORTS:
-                this.classElement = sidebarTypes.GAMEREPORTS.classElement;
-                this.icon = sidebarTypes.GAMEREPORTS.icon;
-                this.text = sidebarTypes.GAMEREPORTS.text;
-                break;
-            case sidebarTypes.RELEASES:
-                this.classElement = sidebarTypes.RELEASES.classElement;
-                this.icon = sidebarTypes.RELEASES.icon;
-                this.text = sidebarTypes.RELEASES.text;
-                break;
-            case sidebarTypes.USERS:
-                this.classElement = sidebarTypes.USERS.classElement;
-                this.icon = sidebarTypes.USERS.icon;
-                this.text = sidebarTypes.USERS.text;
-                break;
-            default:
-                break;
-        }
-    }
-
     handleClick() {
-        this.props.onClick(this.props.itemType);
+        this.props.onClick(this.text);
     }
 
     render() {
