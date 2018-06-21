f77d02235135bf8f7a25d8bb176591bb353ac620
AttilaBalla <xattus@gmail.com>
2018-06-21 22:46:18 +0200

    Newsfeed on index page is now dynamic

--- src/js/core/newsfeed/Newsfeed.js
+++ src/js/core/newsfeed/Newsfeed.js
@@ -1,13 +1,42 @@
 import React from "react";
 import {NewsFeedPost} from "./NewsfeedPost";
+import {getAllNewsPosts} from "../../util/APIUtils";
 
 export class Newsfeed extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {newsPosts: []};
+    }
+
+    componentDidMount() {
+        getAllNewsPosts()
+            .then(response => {
+                this.setState({newsPosts: response})
+            })
+            .catch(error => {
+                console.log(error);
+                //TODO ERROR HANDLING
+            })
+    }
 
     render() {
         return (
             <div className="col-9 text_box">
-                <NewsFeedPost/>
-                <NewsFeedPost/>
+            {this.state.newsPosts.map((newsPost, key) => {
+
+                return (
+                    (newsPost.visible === "True") ?
+                    <NewsFeedPost
+                        key={key}
+                        title={newsPost.title}
+                        owner={newsPost.owner}
+                        date={newsPost.date}
+                        content={newsPost.content}
+                    /> : null
+                )
+            })}
             </div>
         )
     }
